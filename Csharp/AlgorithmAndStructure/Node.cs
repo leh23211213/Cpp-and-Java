01c229bdeba8607515fb5291a4c51322edf0e281
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2023-12-17 21:47:14 +0700

    update linked list features

--- Csharp/AlgorithmAndStructure/Node.cs
+++ Csharp/AlgorithmAndStructure/Node.cs
@@ -1,3 +1,6 @@
+using System.Runtime.CompilerServices;
+using System.Xml;
+
 public class Node<T> where T : IComparable
 {
     public T Data { get; set; }
@@ -7,36 +10,98 @@ public class Node<T> where T : IComparable
         Data = default;
         Next = null;
     }
-    public Node(T emplement)
+    public Node(T element)
     {
-        Data = emplement;
+        Data = element;
         Next = null;
     }
 }
 public class LinkedList<T> where T : IComparable
 {
-    public LinkedList(){
-    }
-    public LinkedList(T implement){
-        head.Data = implement;
-    }
     public Node<T> head { get; set; } = new Node<T>();
-    public void Print(){
+    public LinkedList()
+    { }
+    public LinkedList(T element)
+    {
+        head.Data = element;
+        head.Next = null;
+    }
+    public void Print()
+    {
         var list = head;
         while (list != null)
         {
-            Console.WriteLine(list.Data);
+            Console.Write(list.Data + " -> ");
             list = list.Next;
         }
     }
-
-    public Node<T> Find(T Data)
+    public Node<T> Find(T data)
     {
-        var list = head;
-        while (list != null && list.Data?.CompareTo(Data) != 0)
-            list = head.Next;
-        if (list != head)
-            return list;
+        if (head == null || head.Data.CompareTo(data) == 0)
+        {
+            return null;
+        }
+
+        var currenNode = head;
+        while (currenNode != null && currenNode.Data?.CompareTo(data) != 0)
+        {
+            currenNode = currenNode.Next;
+        }
+        if (currenNode != head)
+            return currenNode;
         return null;
+    }
+    public Node<T> FindPrevious(T data)
+    {
+        if (head == null || head.Data.CompareTo(data) == 0)
+        {
+            return null;
+        }
+
+        var currenNode = head;
+        while (currenNode != null && currenNode.Next?.Data.CompareTo(data) != 0)
+        {
+            currenNode = currenNode.Next;
+        }
+        if (currenNode != head)
+            return currenNode;
+        return null;
+    }
+    public Node<T> Insert(T data, T afterValue)
+    {
+        var currenNode = Find(afterValue);
+        var newNode = new Node<T>(data);
+        if (currenNode != null)
+        {
+            newNode.Next = currenNode.Next;
+            currenNode.Next = newNode;
+            return currenNode;
+        }
+        return null;
+    }
+    public Node<T> Add(T Data)
+    {
+        var currenNode = head;
+        if (currenNode == null)
+        {
+            return new Node<T>(Data);
+        }
+        while(currenNode.Next != null)
+        {
+            currenNode = currenNode.Next;
+        }
+        var newNode = new Node<T>(Data);
+        currenNode.Next = newNode;
+        return currenNode;
+    }
+
+    public void Traverse(Action<T> action)
+    {
+        var currenNode = head;
+        while (currenNode.Next != null)
+        {
+            action?.Invoke(currenNode.Next.Data);
+            currenNode = currenNode.Next;
+        }
     }
 }
--- Csharp/AlgorithmAndStructure/Program.cs
+++ Csharp/AlgorithmAndStructure/Program.cs
@@ -3,14 +3,22 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        LinkedList<int> a = new LinkedList<int>(1);
-        a.head.Data = 11;
-        Node<int> b = new Node<int>(1);
-        Node<int> c = new Node<int>(2);
-        Node<int> d = new Node<int>(3);
+        LinkedList<int> a = new LinkedList<int>(-1);
+        Node<int> b = new Node<int> { Data = 0 };
+        Node<int> c = new Node<int> { Data = 1 };
+        Node<int> d = new Node<int> { Data = 2 };
+        Node<int> e = new Node<int>(3);
+        Node<int> f = new Node<int>(4);
+        Node<int> g = new Node<int>(5);
         a.head.Next = b;
         b.Next = c;
         c.Next = d;
+        d.Next = e;
+        e.Next = f;
+        f.Next = g;
+        a.Insert(55, 2);
+        Console.WriteLine();
+        a.Add(9);
         a.Print();
     }
 }
